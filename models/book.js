b9c9a36b943e82b40b44b29e03f52f4bf0740485
Wasim <wasim.sayyed@cuelogic.com>
2016-12-09 17:25:40 +0530

    Added source files

---
+++ models/book.js
@@ -0,0 +1,55 @@
+var mongoose = require('mongoose');
+
+//Genere Schema
+var bookSchema = mongoose.Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    genre:{
+        type: String,
+        required: true
+    },
+    description:{
+        type: String                
+    },
+    author:{
+        type: String,
+        required: true
+    },
+    publisher:{
+        type: String        
+    },
+    pages:{
+        type: String
+    },
+    image_url:{
+        type: String        
+    },
+    buy_url:{
+        type: String        
+    },
+    create_date:{
+        type: Date,
+        default: Date.now
+    }
+});
+
+var Book = module.exports = mongoose.model('Book',bookSchema);
+
+
+//Get Books 
+module.exports.getBooks = function(callback,limit) {
+    Book.find(callback).limit(limit);
+}
+
+
+//Get Book 
+module.exports.getBookById = function(id,callback) {
+    Book.findById(id,callback);
+}
+
+//Add Books 
+module.exports.addBook = function(book,callback) {
+    Book.create(book, callback);
+}
---
+++ models/genre.js
@@ -0,0 +1,36 @@
+var mongoose = require('mongoose');
+
+//Genere Schema
+var genreSchema = mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    create_date:{
+        type: Date,
+        default: Date.now
+    }
+});
+
+var Genre = module.exports = mongoose.model('Genre',genreSchema);
+
+
+//Get genres 
+module.exports.getGenres = function(callback,limit) {
+    Genre.find(callback).limit(limit);
+}
+
+//Add genre
+module.exports.addGenre = function(genre,callback) {
+    Genre.create(genre, callback);
+}
+
+
+//Update genre
+module.exports.updateGenre = function(id, genre, options, callback) {
+    var query = {_id: id };
+    var update = {
+        name: genre.name
+    }
+    Genre.findOneAndUpdate(query, update, options, callback);
+}
